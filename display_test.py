82aaa901bcd0bb214e8013a5e656cea5731a3a0e
Dan Snyder <snyderdan46@gmail.com>
2013-11-03 17:32:10 -0500

    Initial commit

---
+++ display_test.py
@@ -0,0 +1,12 @@
+import pygame
+from PyVector import Vector
+
+pygame.init()
+
+screen = pygame.display.set_mode((500,500))
+
+screen.fill((255,255,255))
+
+while True:
+    pygame.display.flip()
+pygame.quit()
