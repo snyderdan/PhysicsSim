e7ff7a5a1cfd0e0a0b77a34f57fe8a298a8048c5
Dan Snyder <snyderdan46@gmail.com>
2013-11-04 18:34:37 -0500

    still working with 2D displays

--- display_test.py
+++ display_test.py
@@ -24,18 +24,26 @@ def renderVector2D(v, scale=1, frame=(ihat,jhat), col=(255,0,0)):
     
     image.fill((255,255,255))
     image.blit(font.render(text, True, (0,0,0)), (0,0))
-    pygame.draw.line(image, (0,0,0), (wh/2*scale,0), (wh/2*scale,wh*scale), 1)
-    pygame.draw.line(image, (0,0,0), (0,wh/2*scale), (wh*scale,wh/2*scale), 1)
-    pygame.draw.line(image, col, (wh/2*scale, wh/2*scale), ((wh/2+v.components[frame[0]])*scale, (wh/2-v.components[frame[1]])*scale))
+    pygame.draw.line(image, (0,0,0), (wh/2*scale,0), (wh/2*scale,wh*scale), 2)
+    pygame.draw.line(image, (0,0,0), (0,wh/2*scale), (wh*scale,wh/2*scale), 2)
+    pygame.draw.line(image, col, (wh/2*scale, wh/2*scale), ((wh/2+v.components[frame[0]])*scale, (wh/2-v.components[frame[1]])*scale), 2)
 
     return image
+
+def renderVector3D(v, scale=1, col=(255,0,0)):
+    font = pygame.font.SysFont("Calibri",14)
+    wh   = int(v.length())*2
+    text = "i, j, k"
+    image = pygame.Surface((wh*scale, wh*scale))
 
 pygame.init()
 
 screen = pygame.display.set_mode((500,500))
-vector = Vector((22.5,7.8,-2))
+vector = Vector((22.5,7.8,-34))
+rotate = Vector((0,0,1))
 frames = ((ihat, jhat), (khat, jhat), (ihat, khat))
 index  = 0
+timer  = pygame.time.Clock()
 
 screen.fill((255,255,255))
 
@@ -51,8 +59,14 @@ while running:
                 img   = renderVector2D(vector, scale=4, frame=frames[index])
                 index = (index + 1) % 3
                 screen.blit(img, (screen.get_width()/2-img.get_width()/2, screen.get_height()/2-img.get_height()/2))
+            elif event.key == K_DOWN:
+                screen.fill((255,255,255))
+                img   = renderVector2D(vector, scale=4, frame=frames[index])
+                index = (index - 1) % 3
+                screen.blit(img, (screen.get_width()/2-img.get_width()/2, screen.get_height()/2-img.get_height()/2))
         elif event.type == QUIT:
             running = False
-            
+    vector.rotate(rotate)
     pygame.display.flip()
+    timer.tick(30)
 pygame.quit()
